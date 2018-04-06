1d204065dafa53bc9d6983cd411befbd7d9a8131
Alexander Urizar <urizaraj@gmail.com>
2018-04-06 15:52:51 -0400

    Use an ooo aproach to updating DOM

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -1,35 +1,72 @@
 const branches = $('#branches')
-let res
-
 const curTitle = $('#curTitle')
 const curBody = $('#curBody')
+const sid = 1
 
-$(() => {
-  branches.on('click', getBranch)
-})
+class Application {
+  constructor() {
+    this.curBranches = branches.children().toArray().map(el => {
+      const id = el.dataset.id
+      const title = el.text
+      return new Branch(id, title)
+    })
+  }
 
-function getBranch(event) {
-  event.preventDefault()
-
-  const id = event.target.dataset.id
-  const sid = event.target.dataset.sid
-
-  resp = $.get(`/stories/${sid}/branches/${id}`)
-  resp.done(data => {
-    res = data
-    curTitle.text(data.title)
-    curBody.text(data.body)
-    buildBranches(data.branches)
-  })
+  getBranch(event) {
+    event.preventDefault()
+    const id = event.currentTarget.dataset.id
+    this.curBranches.find(b => b.id == id).load()
+  }
 }
 
-function buildBranches(array) {
-  branches.empty()
-  array.forEach(branch => {
-    let link = $('#hidden').children().clone()
-    let h = link.children()
-    h[0].dataset.id = branch.id
-    h.text(branch.title)
-    branches.append(link)
+class Branch {
+  constructor(id, title) {
+    this.id = id
+    this.title = title
+  }
+
+  load() {
+    let resp = $.get(`/stories/${sid}/branches/${this.id}`)
+    resp.done(data => {
+      this.body = data.body
+      this.branches = data.branches.map(b => {
+        const id = b.id
+        const title = b.title
+        return new Branch(id, title)
+      })
+      this.display()
+    })
+  }
+
+  display() {
+    curTitle.text(this.title)
+    curBody.text(this.body)
+
+    app.curBranches = this.branches
+    this.addLinks()
+    setBranchListeners()
+  } 
+
+  addLinks() {
+    branches.empty()
+    this.branches.forEach(b => {
+      let link = $('#hidden').children().clone()
+      link[0].dataset.id = b.id
+      let h = link.children()
+      h.text(b.title)
+      branches.append(link)
+    })
+  }
+}
+
+const app = new Application()
+
+$(() => {
+  setBranchListeners()
+})
+
+function setBranchListeners() {
+  branches.children().toArray().forEach(a => {
+    $(a).on('click', event => app.getBranch(event))
   })
 }
