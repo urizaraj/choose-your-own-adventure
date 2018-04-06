49655cb4bc8cddb4a2391aee0bd657cf12d9e304
Alexander Urizar <urizaraj@gmail.com>
2018-04-06 12:29:33 -0400

    Configure model associations

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,2 +1,4 @@
 class Branch < ApplicationRecord
+  belongs_to :parent, class_name: 'Branch', optional: true
+  has_many :branches, foreign_key: 'parent_id'
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,4 @@
 class Story < ApplicationRecord
+  has_one :start_branch, class_name: 'Branch'
+  has_many :branches
 end
