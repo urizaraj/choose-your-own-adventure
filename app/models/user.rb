39c55df7213893a98cfc224d80adfb4b8aaf274a
Alexander Urizar <urizaraj@gmail.com>
2018-04-06 10:12:36 -0400

    Migrate users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, omniauth_providers: %i[github]
 
-  has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
 
@@ -17,19 +15,6 @@ class User < ApplicationRecord
       where(conditions.to_h).where(['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]).first
     elsif conditions.key?(:name) || conditions.key?(:email)
       where(conditions.to_h).first
-    end
-  end
-
-  def reviewed_restaurant?(restaurant)
-    reviews.find_by_restaurant_id(restaurant.id)
-  end
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
-      # user.image = auth.info.image
     end
   end
 end
