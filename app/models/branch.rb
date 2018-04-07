970a9539643f46cd85c18cbff6aa2f2cee7b1dbd
Alexander Urizar <urizaraj@gmail.com>
2018-04-07 13:47:11 -0400

    Branches belong to a story, stories belong to a start_branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,5 @@
 class Branch < ApplicationRecord
   belongs_to :parent, class_name: 'Branch', optional: true
   has_many :branches, foreign_key: 'parent_id'
+  belongs_to :story
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,4 @@
 class Story < ApplicationRecord
-  has_one :start_branch, class_name: 'Branch'
+  belongs_to :start_branch, class_name: 'Branch'
   has_many :branches
 end
