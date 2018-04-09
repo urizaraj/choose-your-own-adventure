1fd1ba8ddc334a7157121d521745181372506702
Alexander Urizar <urizaraj@gmail.com>
2018-04-09 16:13:05 -0400

    Add story creation functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,4 +6,25 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find(params[:id])
   end
+
+  def new
+    @story = Story.new
+    @branch = @story.build_start_branch
+  end
+
+  def create
+    story = Story.new(strong_params)
+    branch = story.start_branch
+    story.user = current_user
+    branch.user = current_user
+    branch.story = story
+    return render :new unless story.save
+    redirect_to stories_path
+  end
+
+  def strong_params
+    params
+      .require(:story)
+      .permit(:title, :description, start_branch_attributes: %i[title body])
+  end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,6 @@ class Story < ApplicationRecord
   belongs_to :start_branch, class_name: 'Branch'
   belongs_to :user
   has_many :branches
+
+  accepts_nested_attributes_for :start_branch
 end
