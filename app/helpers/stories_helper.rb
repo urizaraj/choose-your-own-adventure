aeaa8b2f394a60b8897bbc9723f158f6cd05e0a7
Alexander Urizar <urizaraj@gmail.com>
2018-04-10 16:28:58 -0400

    Add a story head helper

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,3 +1,12 @@
 module StoriesHelper
-
+  def story_head(story)
+    tag.h1 story.title,
+           id: 'storyTitle',
+           class: 'mb-0',
+           data: {
+             id: story.id,
+             sbid: story.start_branch.id,
+             sbtitle: story.start_branch.title
+           }
+  end
 end
