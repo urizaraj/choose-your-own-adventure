ca62fb56f2d5c02238c2989d7da274a1ef877f01
Alexander Urizar <urizaraj@gmail.com>
2018-04-12 12:23:37 -0400

    Style updates

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,7 +2,7 @@ module StoriesHelper
   def story_head(story)
     tag.h1 story.title,
            id: 'storyTitle',
-           class: 'mb-0',
+           class: 'mb-0 text-secondary',
            data: {
              id: story.id,
              sbid: story.start_branch.id,
