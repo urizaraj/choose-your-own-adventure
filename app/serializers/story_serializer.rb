0ef0dc7af1fba3c1350521ea4a5318f1a96f9943
Alexander Urizar <urizaraj@gmail.com>
2018-04-17 17:52:42 -0400

    Story serializer does not return uneeded information about branches

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,4 +1,8 @@
 class StorySerializer < ActiveModel::Serializer
   attributes :id, :title, :description
   has_many :branches
+
+  class BranchSerializer < ActiveModel::Serializer
+    attributes :id, :end
+  end
 end
